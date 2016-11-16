25ad513023018463983e731c9798ff501b18bda5
grahamcrowell <graham.crowell@gmail.com>
2016-11-15 18:08:40 -0800

    copy from SqlUtilities V1

---
+++ SqlServerUtilities/EtlPackage/Utilities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EtlPackage
+{
+    public static class Utilities
+    {
+        public static void StartUp()
+        {
+
+        }
+        public static void ShutDown()
+        {
+
+        }
+        public static String Cwd()
+        {
+            string path = Directory.GetCurrentDirectory();
+            Console.WriteLine("The current directory is {0}", path);
+            return path;
+        }
+        public static String Cwd(String newDir)
+        {
+            if (!Directory.Exists(newDir))
+            {
+                Directory.CreateDirectory(newDir);
+            }
+            Directory.SetCurrentDirectory(newDir);
+            return Cwd();
+        }
+        public static void user_exit()
+        {
+            Console.WriteLine("\r\n\r\nexecution complete\r\npress enter key to exit");
+            Console.ReadKey();
+            //while (Console.ReadKey().Key != ConsoleKey.Escape && Console.ReadKey().Key != ConsoleKey.Enter) { }
+        }
+
+    }
+}
